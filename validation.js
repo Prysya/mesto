7da16d1cf11901ef2d6066cdaa8c879cd8cba1ac
Prysya <bloodofabyss@yandex.ru>
2020-05-11 17:10:18 +0300

    Create validation module

---
+++ validation.js
@@ -0,0 +1,117 @@
+const formValidation = () => {
+  const errors = {
+    ru: {
+      emptyInput: "Это обязательное поле",
+      outOfRange: "Должно быть от 2 до 30 символов",
+      correctInput: "",
+      invalidLink: "Здесь должна быть ссылка",
+    },
+  };
+
+  const getFormElements = (event) => event.currentTarget.elements;
+
+  const checkEmptyInput = (event, ...inputs) => {
+    if (event.target.value.length === 0) {
+      inputs.forEach((input) => {
+        if (event.target.name === input.name) {
+          document.querySelector(`#${input.name}`).textContent =
+            errors.ru.emptyInput;
+        }
+      });
+    }
+  };
+
+  const checkRange = (event, ...inputs) => {
+    if (event.target.value.length === 1 || event.target.value.length > 30) {
+      inputs.forEach((input) => {
+        if (event.target.name === input.name) {
+          document.querySelector(`#${input.name}`).textContent =
+            errors.ru.outOfRange;
+        }
+      });
+    }
+  };
+
+  const checkCorrectInput = (event, ...inputs) => {
+    if (event.target.validity.valid) {
+      inputs.forEach((input) => {
+        if (event.target.name === input.name) {
+          document.querySelector(`#${input.name}`).textContent =
+            errors.ru.correctInput;
+        }
+      });
+    }
+  };
+
+  const checkLink = (event, ...inputs) => {
+    if (
+      !event.target.validity.valid &&
+      event.target.value.length === 0
+    ) {
+      inputs.forEach((input) => {
+        if (event.target.name === input.name) {
+          document.querySelector(`#${input.name}`).textContent =
+            errors.ru.emptyInput;
+        }
+      });
+    } else if (
+      !event.target.validity.valid &&
+      event.target.name === "placeLink"
+    ) {
+      inputs.forEach((input) => {
+        if (event.target.name === input.name) {
+          document.querySelector(`#${input.name}`).textContent =
+            errors.ru.invalidLink;
+        }
+      });
+    }
+  };
+
+  const disabledButton = (event) =>
+    event.currentTarget
+      .querySelector(".popup__button")
+      .setAttribute("disabled", true);
+
+  const activateButton = (event) =>
+    event.currentTarget
+      .querySelector(".popup__button")
+      .removeAttribute("disabled");
+
+  const removeErrors = (event) =>
+    event.currentTarget
+      .querySelectorAll(".popup__input-error")
+      .forEach((error) => (error.textContent = ""));
+
+  const validationPlace = (event) => {
+    const { placeName, placeLink } = getFormElements(event);
+
+    if (!placeName.validity.valid || !placeLink.validity.valid) {
+      checkEmptyInput(event, placeName, placeLink);
+      checkRange(event, placeName);
+      checkCorrectInput(event, placeName, placeLink);
+      checkLink(event, placeLink);
+      disabledButton(event);
+    } else {
+      removeErrors(event);
+      activateButton(event);
+    }
+  };
+
+  const validationEdit = (event) => {
+    const { editName, editAbout } = getFormElements(event);
+
+    if (!editName.validity.valid || !editAbout.validity.valid) {
+      checkEmptyInput(event, editName, editAbout);
+      checkRange(event, editName, editAbout);
+      checkCorrectInput(event, editName, editAbout);
+      disabledButton(event);
+    } else {
+      removeErrors(event);
+      activateButton(event);
+    }
+  }
+
+  return {validationPlace, validationEdit}
+};
+
+export default formValidation;
